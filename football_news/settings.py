17c8fed0fd9106da2cfd92d60cda5e9d5ec16b50
Amar Hakim <amarhakimhamzah@gmail.com>
2025-08-27 16:18:14 +0700

    Tutorial 0

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ugz3ihb9ad-kkpe%y=g&umhqn&%qwg$-+j6wo(6rfl7@tvly1g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "amar-hakim-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
