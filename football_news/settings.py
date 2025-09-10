f907065833a288c09f26727943e6512557943bab
Amar Hakim <amarhakimhamzah@gmail.com>
2025-09-10 16:07:26 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "amar-hakim-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://amar-hakim-footballnews.pbp.cs.ui.ac.id/"
+    "https://amar-hakim-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
